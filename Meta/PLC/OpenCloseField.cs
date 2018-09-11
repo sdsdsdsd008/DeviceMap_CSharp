bb709d10cf4dd48ae0b275e597007b5885055651
jialiang.li <80201288@qq.com>
2018-09-11 15:26:40 +0800

    增加开关量点位类型

---
+++ Meta/PLC/OpenCloseField.cs
@@ -0,0 +1,21 @@
+﻿namespace SdcSoft.Devices.Meta.PLC
+{
+    internal class OpenCloseField : Meta.OpenCloseField
+    {
+        public override bool haveValue(params byte[] bytes)
+        {
+            value = bytes[1] << 8 | bytes[0];
+            int i = 1 << bit;
+            if ((i & value) == i)
+            {
+                value = 1;
+                return true;
+            }
+            else
+            {
+                value = 0;
+                return false;
+            }
+        }
+    }
+}
